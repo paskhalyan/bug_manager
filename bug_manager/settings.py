b9a8f1d96937927f01a1ec4a2615644403792f42
paskhalyan <paskhalyan@gmail.com>
2023-02-10 15:08:03 +0400

    get django secret key from os

--- bug_manager/settings.py
+++ bug_manager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l29s#-pq8b(9kb)7%r2!vag*8vd$e4!c#b)2h#&tk5=-zl*wg$'
+SECRET_KEY = os.getenv('django_secret_key', 'django-insecure-+1!$%&*()_+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
